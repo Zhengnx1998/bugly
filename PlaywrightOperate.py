884dcdf8ff1037cae5a11882ae4610fbdddbd861
songweixin <songweixin@wangxiaobao.com>
2023-11-07 16:28:47 +0800

    将产品近7天的功能抽象出来

--- PlaywrightOperate.py
+++ PlaywrightOperate.py
@@ -4,16 +4,14 @@
 # @Email : znx_969@163.com
 # @File : test.py
 # @Project : bugly_test
-import datetime
+
 import os.path
-import re
 import time
 
 from playwright.sync_api import sync_playwright
 
 import common
 import dingding
-import smms
 
 
 class PyPage:
@@ -25,17 +23,10 @@ class PyPage:
         self.browser = None
         self.page = None
         self.isLogin = True
-        self.smmss = smms.Smms(app)
         self.ding = dingding.DingDing(app)
         self.app = app
 
     def login_by(self) -> None:
-        if len(self.app.config['url_hash_list']) != 0:
-            for hash_list in self.app.config['url_hash_list']:
-                self.smmss.delete_image_url(hash_list)
-            self.app.config['url_hash_list'] = []
-        # if self.isOpenBrowser is False:
-        # self.playwright = sync_playwright().start()
         with sync_playwright() as self.playwright:
             self.browser = self.playwright.chromium.launch(headless=True)
             if os.path.exists("state.json"):
@@ -63,113 +54,109 @@ class PyPage:
                         break
                 time.sleep(2)
                 self.app.logger.info("上传登录二维码")
-                qr.screenshot(path='./image/qr.png')
-                self.smmss.get_token()
-                self.smmss.upload_image('./image/qr.png')
+                qr.screenshot(path='/www/wwwroot/47.93.62.235/buglyImage/qr.png')
                 self.app.logger.info("发送登录二维码")
-                self.ding.dingding_robot(self.smmss.image_url)
-                time.sleep(10)
-                # self.app.config['url_hash_list'].append(self.smmss.hash)
-                common.tool_list(self.smmss.hash, self.app.config['url_hash_list'])
+                self.ding.dingding_robot("http://47.93.62.235/buglyImage/qr.png")
                 self.app.logger.info("结束二维码登录")
                 self.isLogin = True
                 # 登录成功
             if self.isLogin:
+                now_product_name = None
                 self.app.logger.info("执行电视家业务动作")
-                self.select_dsj_tv()
-                self.context.storage_state(path="state.json")
-                time.sleep(5)
+                now_product_name = common.select_product(self.page, self.app, self.ding, "电视家3_0", now_product_name,
+                                                         0.65)
                 self.app.logger.info("执行安卓业务动作")
-                self.select_android()
+                common.select_product(self.page, self.app, self.ding, "电视家随身版", now_product_name,
+                                      0.30)
                 self.context.storage_state(path="state.json")
             else:
                 self.app.logger.info("未登录不执行")
 
     # 电视家3.0
-    def select_dsj_tv(self):
-        self.page.get_by_text("电视家3_0").click()
-        # 判断x元素是否存在
-        if self.page.get_by_label("Close", exact=True).count() > 0:
-            self.page.get_by_label("Close", exact=True).click()
-        # 切换到7天的数据
-        max_dsj_tv_crash = common.json_handle(self.page, self.app, "电视家3_0")
-        self.page.locator("div").filter(has_text=re.compile(r"^今天$")).nth(1).click()
-        self.page.get_by_text("最近7天").click()
-        # 判断崩溃率是否大于0.65或者当前时间=18点0几分
-        if (datetime.datetime.now().hour == 18 and datetime.datetime.now().minute // 10 == 0) or float(
-                max_dsj_tv_crash) > 0.65:
-            # 截图
-            time.sleep(5)
-            dsj_tv_image = self.page.locator(
-                "xpath=//*[@class=\"_3QsyGr2hqAZjVh6fZnzI6t _3wcN4SQOWbN86r7emNH6FU _1PKKo_a_rrd5QsVWTXa1ax\"]").first
-            dsj_tv_image.screenshot(path='./image/dsj_tv_image.png')
-            self.smmss.get_token()
-            self.smmss.upload_image('./image/dsj_tv_image.png')
-            dsj_tv_image_url = self.smmss.image_url
-            dsj_tv_image_url_hash = self.smmss.hash
-            time.sleep(5)
-            # 截图top问题table
-            dsj_tv_bug_image = self.page.locator("xpath=/html/body/div/div/div/div[2]/div/div[2]/div[3]")
-            dsj_tv_bug_image.screenshot(path='./image/dsj_tv_bug_image.png')
-            self.smmss.get_token()
-            self.smmss.upload_image('./image/dsj_tv_bug_image.png')
-            dsj_tv_bug_image_url = self.smmss.image_url
-            dsj_tv_bug_image_url_hash = self.smmss.hash
-            self.ding.dingding_robot_text("电视家TV端近6小时最高崩溃率为:", str(max_dsj_tv_crash),
-                                          ["@18780106625", "@13618035171", "@18380473531", "@19138982495",
-                                           "@15360584721"],
-                                          [dsj_tv_image_url, dsj_tv_bug_image_url])
-            time.sleep(5)
-            print(f"告警截图哈希值{dsj_tv_image_url_hash}")
-            common.tool_list(dsj_tv_image_url_hash, self.app.config['url_hash_list'])
-            time.sleep(1)
-            print(f"top截图哈希值{dsj_tv_bug_image_url_hash}")
-            common.tool_list(dsj_tv_bug_image_url_hash, self.app.config['url_hash_list'])
-        else:
-            self.app.logger.info("tv端崩溃率小于0.65,且没有到6点不发送告警")
-
-    # 安卓业务
-    def select_android(self):
-        self.page.get_by_text("电视家3_0").click()
-        self.page.get_by_text("电视家随身版").click()
-        # 判断x元素是否存在
-        if self.page.get_by_label("Close", exact=True).count() > 0:
-            self.page.get_by_label("Close", exact=True).click()
-        max_dsj_android = common.json_handle(self.page, self.app, "电视家随身版")
-        time.sleep(5)
-        self.page.locator("div").filter(has_text=re.compile(r"^今天$")).nth(1).click()
-        self.page.get_by_text("最近7天").click()
-        # 判断崩溃率是否大于0.30或者当前时间=18点0几分
-        if (datetime.datetime.now().hour == 18 and datetime.datetime.now().minute // 10 == 0) or float(
-                max_dsj_android) > 0.30:
-            # 切换到7天的数据截图
-            time.sleep(5)
-            dsj_android_image = self.page.locator(
-                "xpath=//*[@class=\"_3QsyGr2hqAZjVh6fZnzI6t _3wcN4SQOWbN86r7emNH6FU _1PKKo_a_rrd5QsVWTXa1ax\"]").first
-            dsj_android_image.screenshot(path='./image/dsj_android_image.png')
-            self.smmss.get_token()
-            self.smmss.upload_image('./image/dsj_android_image.png')
-            dsj_android_image_url = self.smmss.image_url
-            dsj_android_image_url_hash = self.smmss.hash
-            time.sleep(5)
-            # 截图top问题table
-            dsj_android_bug_image = self.page.locator("xpath=/html/body/div/div/div/div[2]/div/div[2]/div[3]")
-            dsj_android_bug_image.screenshot(path='./image/dsj_android_bug_image.png')
-            self.smmss.get_token()
-            self.smmss.upload_image('./image/dsj_android_bug_image.png')
-            dsj_android_bug_image_url = self.smmss.image_url
-            dsj_android_bug_image_url_hash = self.smmss.hash
-            self.ding.dingding_robot_text("电视家安卓近6小时内崩溃率最高为:", str(max_dsj_android),
-                                          ["@18780106625", "@15680059975", "@18502828246", "@15669027751"],
-                                          [dsj_android_image_url, dsj_android_bug_image_url])
-            time.sleep(5)
-            time.sleep(5)
-            print(f"告警截图哈希值{dsj_android_image_url_hash}")
-            # self.app.config['url_hash_list'].append(dsj_android_image_url_hash)
-            common.tool_list(dsj_android_image_url_hash, self.app.config['url_hash_list'])
-            time.sleep(1)
-            print(f"top截图哈希值{dsj_android_bug_image_url_hash}")
-            # self.app.config['url_hash_list'].append(dsj_android_bug_image_url_hash)
-            common.tool_list(dsj_android_bug_image_url_hash, self.app.config['url_hash_list'])
-        else:
-            self.app.logger.info("安卓崩溃率小于0.30,且没有到6点，不发送告警")
+    # def select_dsj_tv(self):
+    #     self.page.get_by_text("电视家3_0").click()
+    #     # 判断x元素是否存在
+    #     if self.page.get_by_label("Close", exact=True).count() > 0:
+    #         self.page.get_by_label("Close", exact=True).click()
+    #     # 切换到7天的数据
+    #     max_dsj_tv_crash = common.json_handle(self.page, self.app, "电视家3_0")
+    #     self.page.locator("div").filter(has_text=re.compile(r"^今天$")).nth(1).click()
+    #     self.page.get_by_text("最近7天").click()
+    #     # 判断崩溃率是否大于0.65或者当前时间=18点0几分
+    #     if (datetime.datetime.now().hour == 18 and datetime.datetime.now().minute // 10 == 0) or float(
+    #             max_dsj_tv_crash) > 0.65:
+    #         # 截图
+    #         time.sleep(5)
+    #         dsj_tv_image = self.page.locator(
+    #             "xpath=//*[@class=\"_3QsyGr2hqAZjVh6fZnzI6t _3wcN4SQOWbN86r7emNH6FU _1PKKo_a_rrd5QsVWTXa1ax\"]").first
+    #         dsj_tv_image.screenshot(path='./image/dsj_tv_image.png')
+    #         self.smmss.get_token()
+    #         self.smmss.upload_image('./image/dsj_tv_image.png')
+    #         dsj_tv_image_url = self.smmss.image_url
+    #         dsj_tv_image_url_hash = self.smmss.hash
+    #         time.sleep(5)
+    #         # 截图top问题table
+    #         dsj_tv_bug_image = self.page.locator("xpath=/html/body/div/div/div/div[2]/div/div[2]/div[3]")
+    #         dsj_tv_bug_image.screenshot(path='./image/dsj_tv_bug_image.png')
+    #         self.smmss.get_token()
+    #         self.smmss.upload_image('./image/dsj_tv_bug_image.png')
+    #         dsj_tv_bug_image_url = self.smmss.image_url
+    #         dsj_tv_bug_image_url_hash = self.smmss.hash
+    #         self.ding.dingding_robot_text("电视家TV端近6小时最高崩溃率为:", str(max_dsj_tv_crash),
+    #                                       ["@18780106625", "@13618035171", "@18380473531", "@19138982495",
+    #                                        "@15360584721"],
+    #                                       [dsj_tv_image_url, dsj_tv_bug_image_url])
+    #         time.sleep(5)
+    #         print(f"告警截图哈希值{dsj_tv_image_url_hash}")
+    #         common.tool_list(dsj_tv_image_url_hash, self.app.config['url_hash_list'])
+    #         time.sleep(1)
+    #         print(f"top截图哈希值{dsj_tv_bug_image_url_hash}")
+    #         common.tool_list(dsj_tv_bug_image_url_hash, self.app.config['url_hash_list'])
+    #     else:
+    #         self.app.logger.info("tv端崩溃率小于0.65,且没有到6点不发送告警")
+    #
+    # # 安卓业务
+    # def select_android(self):
+    #     self.page.get_by_text("电视家3_0").click()
+    #     self.page.get_by_text("电视家随身版").click()
+    #     # 判断x元素是否存在
+    #     if self.page.get_by_label("Close", exact=True).count() > 0:
+    #         self.page.get_by_label("Close", exact=True).click()
+    #     max_dsj_android = common.json_handle(self.page, self.app, "电视家随身版")
+    #     time.sleep(5)
+    #     self.page.locator("div").filter(has_text=re.compile(r"^今天$")).nth(1).click()
+    #     self.page.get_by_text("最近7天").click()
+    #     # 判断崩溃率是否大于0.30或者当前时间=18点0几分
+    #     if (datetime.datetime.now().hour == 18 and datetime.datetime.now().minute // 10 == 0) or float(
+    #             max_dsj_android) > 0.30:
+    #         # 切换到7天的数据截图
+    #         time.sleep(5)
+    #         dsj_android_image = self.page.locator(
+    #             "xpath=//*[@class=\"_3QsyGr2hqAZjVh6fZnzI6t _3wcN4SQOWbN86r7emNH6FU _1PKKo_a_rrd5QsVWTXa1ax\"]").first
+    #         dsj_android_image.screenshot(path='./image/dsj_android_image.png')
+    #         self.smmss.get_token()
+    #         self.smmss.upload_image('./image/dsj_android_image.png')
+    #         dsj_android_image_url = self.smmss.image_url
+    #         dsj_android_image_url_hash = self.smmss.hash
+    #         time.sleep(5)
+    #         # 截图top问题table
+    #         dsj_android_bug_image = self.page.locator("xpath=/html/body/div/div/div/div[2]/div/div[2]/div[3]")
+    #         dsj_android_bug_image.screenshot(path='./image/dsj_android_bug_image.png')
+    #         self.smmss.get_token()
+    #         self.smmss.upload_image('./image/dsj_android_bug_image.png')
+    #         dsj_android_bug_image_url = self.smmss.image_url
+    #         dsj_android_bug_image_url_hash = self.smmss.hash
+    #         self.ding.dingding_robot_text("电视家安卓近6小时内崩溃率最高为:", str(max_dsj_android),
+    #                                       ["@18780106625", "@15680059975", "@18502828246", "@15669027751"],
+    #                                       [dsj_android_image_url, dsj_android_bug_image_url])
+    #         time.sleep(5)
+    #         time.sleep(5)
+    #         print(f"告警截图哈希值{dsj_android_image_url_hash}")
+    #         # self.app.config['url_hash_list'].append(dsj_android_image_url_hash)
+    #         common.tool_list(dsj_android_image_url_hash, self.app.config['url_hash_list'])
+    #         time.sleep(1)
+    #         print(f"top截图哈希值{dsj_android_bug_image_url_hash}")
+    #         # self.app.config['url_hash_list'].append(dsj_android_bug_image_url_hash)
+    #         common.tool_list(dsj_android_bug_image_url_hash, self.app.config['url_hash_list'])
+    #     else:
+    #         self.app.logger.info("安卓崩溃率小于0.30,且没有到6点，不发送告警")
--- common.py
+++ common.py
@@ -4,7 +4,15 @@
 # @Email : znx_969@163.com
 # @File : test.py
 # @Project : bugly_test
+import datetime
 import json
+import re
+import time
+
+url = "http://47.93.62.235/buglyImage/"
+path_url = "/www/wwwroot/47.93.62.235/buglyImage/"
+product_dict = {"电视家3_0": ["dsj_tv_image.png", "dsj_tv_bug_image.png", "电视家TV端近6小时最高崩溃率为:"],
+                "电视家随身版": ["dsj_android_image.png", "dsj_android_bug_image.png", "电视家安卓近6小时内崩溃率最高为:"]}
 
 
 def json_handle(page, app, product):
@@ -36,6 +44,34 @@ def json_handle(page, app, product):
         return crash_max
 
 
-def tool_list(hash_url, hash_list):
-    if hash_url not in hash_list:
-        hash_list.append(hash_url)
+def select_product(page, app, ding, product_name, now_product_name, threshold):
+    if now_product_name is not None:
+        page.get_by_text(now_product_name).click()
+    page.get_by_text(product_name).click()
+    # 判断x元素是否存在
+    if page.get_by_label("Close", exact=True).count() > 0:
+        page.get_by_label("Close", exact=True).click()
+    max_dsj = json_handle(page, app, product_name)
+    time.sleep(5)
+    page.locator("div").filter(has_text=re.compile(r"^今天$")).nth(1).click()
+    page.get_by_text("最近7天").click()
+    # 判断崩溃率是否大于阈值或者当前时间=18点0几分
+    if (datetime.datetime.now().hour == 18 and datetime.datetime.now().minute // 10 == 0) or float(
+            max_dsj) > threshold:
+        # 切换到7天的数据截图
+        time.sleep(5)
+        seven_day_image = page.locator(
+            "xpath=//*[@class=\"_3QsyGr2hqAZjVh6fZnzI6t _3wcN4SQOWbN86r7emNH6FU _1PKKo_a_rrd5QsVWTXa1ax\"]").first
+        seven_day_image.screenshot(path=path_url+product_dict.get(product_name)[0])
+        time.sleep(5)
+        # 截图top问题table
+        top_bug_image = page.locator("xpath=/html/body/div/div/div/div[2]/div/div[2]/div[3]")
+        top_bug_image.screenshot(path=path_url+product_dict.get(product_name)[1])
+
+        ding.dingding_robot_text(product_dict.get(product_name)[2], str(max_dsj),
+                                 ["@18780106625", "@15680059975", "@18502828246", "@15669027751"],
+                                 [url+product_dict.get(product_name)[0],
+                                  url+product_dict.get(product_name)[1]])
+    else:
+        app.logger.info(product_name+"小于"+threshold+",且没有到6点，不发送告警")
+    return product_name
